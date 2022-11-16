3ca3d5638de1dadce9fadf77b3799708a189709c
ming.gu <ming.gu@gearradio.net>
2022-11-16 14:34:51 +0800

    Initial Commit

---
+++ ChatFirestore/ChatFirestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatFirestoreApp.swift
+//  ChatFirestore
+//
+//  Created by ming on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct ChatFirestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
